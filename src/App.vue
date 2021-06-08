b3113c0d8ae3a2a51a6081049393fd8c10d8a16b
Ilya <targix8@gmail.com>
2021-06-08 21:30:45 +0700

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,77 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+   <div style="padding: 100px" class="app">
+      <header>
+          <div class="title">
+             <h1>Hyrule Jobs</h1>
+          </div>
+         <div class="order">
+            <button @click="handleClick('title')">order by title</button>
+            <button @click="handleClick('salary')">order by salary</button>
+            <button @click="handleClick('location')">order by location</button>
+         </div>
+      </header>
+     <job-list :jobs="jobs" :order="order"/>
+   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+
+
+import { defineComponent, reactive, ref, toRefs } from 'vue';
+import JobList from './components/JobList.vue';
+import Job from './types/job'
+import OrderTerm from './types/OrderTerm'
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
-  }
+    JobList
+
+  },
+  setup() {
+      const jobs = ref<Job[]>([
+         { title: 'farm worker', location: 'lon lon ranch', salary: 25000, id: '1' },
+         { title: 'quarryman', location: 'death mountain', salary: 30000, id: '2' },
+         { title: 'flute player', location: 'the lost woords', salary: 35000, id: '3' },
+         { title: 'fisherman', location: 'lake hylia', salary: 40000, id: '4' },
+         { title: 'farm worker', location: 'lon lon ranch', salary: 45000, id: '5' }
+      ])
+
+      const order = ref<OrderTerm>('title')
+      const handleClick = (term: OrderTerm) => {
+         order.value = term
+      }
+
+      return {jobs, handleClick, order}
+  },  
+  
+   methods: {
+      
+   }
+
+
 });
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+header {
+   text-align: center;
 }
+
+header .order {
+   margin-top: 20px;
+}
+
+button { 
+   margin: 0 10px;
+   color: #1195c9;
+   border: 3px solid #1195c9;
+   background: #d5f0ff;
+   padding: 8px 16px;
+   border-radius: 4px;
+   cursor: pointer;
+   font-weight: bold;
+}
+
 </style>
---
+++ src/components/JobList.vue
@@ -0,0 +1,89 @@
+<template>
+   <div class="job-list">
+       <p>Ordered by {{order}}</p>
+      <ul>
+         <li v-for="job in orderedJobs" :key="job.id" >
+          <h2>{{job.title}} in {{job.location}}</h2>
+          <div class="salary">
+             <p>{{job.salary}} rupees</p>
+          </div>
+          <div class="description">
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt vitae cumque consectetur aspernatur numquam ipsa voluptatem laboriosam quisquam tempora sequi voluptatibus mollitia hic itaque dolorum neque eius, quo rerum! In?</p>
+          </div>
+          </li>
+      </ul>
+   </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType, computed } from 'vue'
+import Job from '@/types/job'
+import OrderTerm from '@/types/OrderTerm'
+
+export default defineComponent({
+  props: {
+     jobs: {
+        required: true,
+        type: Array as PropType<Job[]>
+     },
+     order: {
+        required: true,
+        type: String as PropType<OrderTerm>
+     }
+  },
+
+  setup(props) {
+     const orderedJobs = computed(() => {
+        return [...props.jobs].sort((a: Job, b: Job) => {
+           return a[props.order] > b[props.order] ? 1 : -1
+        })
+     })
+  return { orderedJobs }
+  }
+
+})
+</script>
+
+
+<style scoped> 
+   .job-list {
+      max-width: 960px;
+      margin: 40px auto;
+   }
+
+   .job-list ul {
+      padding: 0;
+   }
+
+   .job-list li {
+      list-style-type: none;
+      background: white;
+      padding: 16px;
+      margin: 16px 0;
+      border-radius: 4px;
+
+   }
+
+   .job-list h2 {
+      margin: 0 0 10px;
+      text-transform: capitalize;
+   }
+
+   .salary {
+      display: flex;
+   }
+
+   .salary img {
+      width: 30px;
+   }
+
+   .salary p {
+      color: #17bf66;
+      font-weight: bold;
+      margin: 10px 4px;
+   }
+
+
+
+   
+</style>
